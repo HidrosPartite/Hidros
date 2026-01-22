5b9244b344d49370d14d6a6c1af7a40e2c997938
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-01-22 21:29:22 +0100

    Create partite.js

---
+++ partite.js
@@ -0,0 +1,6 @@
+const DB_PARTITE = [
+    { id: '1', titolo: 'Hidros vs Vero Volley', cat: 'Serie C', tag: 'IN EVIDENZA', anno: '12 Gen 2026', punteggio: '3 - 1', img: 'screenshot.png' },
+    { id: '2', titolo: 'Hidros vs Lube Civitanova', cat: 'Under 16', tag: 'FINALE', anno: '08 Gen 2026', punteggio: '3 - 2', img: 'screenshot.png' },
+    { id: '3', titolo: 'Hidros vs Modena Volley', cat: 'Under 14', tag: 'TOP MATCH', anno: '05 Gen 2026', punteggio: '0 - 3', img: 'screenshot.png' },
+    { id: '4', titolo: 'Hidros vs Trentino Itas', cat: 'Serie C', tag: 'REPLAY', anno: '01 Gen 2026', punteggio: '3 - 0', img: 'screenshot.png' }
+];
