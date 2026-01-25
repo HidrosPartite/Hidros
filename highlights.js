b690092a44d7891fe8f6423c3f44310222f454bf
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-01-25 13:42:02 +0100

    Create highlights.js

---
+++ highlights.js
@@ -0,0 +1,6 @@
+const DB_HIGHLIGHTS = [
+    { id: 'h1', titolo: 'Best of Gennaio 2026', cat: 'Highlights', tag: 'TOP', anno: '2026', img: 'h_thumb1.png', slider: 'si' },
+    { id: 'h2', titolo: 'Super Wall - I migliori muri', cat: 'Highlights', tag: 'ACTION', anno: '2026', img: 'h_thumb2.png', slider: 'si' },
+    { id: 'h3', titolo: 'Ace Compilation Serie C', cat: 'Highlights', tag: 'SKILLS', anno: '2026', img: 'h_thumb3.png', slider: 'no' },
+    { id: 'h4', titolo: 'Tie-break al cardiopalma', cat: 'Highlights', tag: 'CLUTCH', anno: '2026', img: 'h_thumb4.png', slider: 'no' }
+];
