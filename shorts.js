6ef1d1bb21c29395848a30beedf5a3b384905e13
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-01-25 14:06:30 +0100

    Update shorts.js

--- shorts.js
+++ shorts.js
@@ -1,6 +1,6 @@
 const DB_SHORTS = [
-    { id: 's1', titolo: 'Riscaldamento Pre-Gara', cat: 'Shorts', tag: 'BEHIND', anno: '2026', img: 's_thumb1.png', sliderHome: 'no' },
-    { id: 's2', titolo: 'Esultanza Sotto la Curva', cat: 'Shorts', tag: 'EMOZIONE', anno: '2026', img: 's_thumb2.png', sliderHome: 'si' },
-    { id: 's3', titolo: 'Punto decisivo U16', cat: 'Shorts', tag: 'MATCH POINT', anno: '2026', img: 's_thumb3.png', sliderHome: 'no' },
-    { id: 's4', titolo: 'Trick Shot in Allenamento', cat: 'Shorts', tag: 'FUN', anno: '2026', img: 's_thumb4.png', sliderHome: 'no' }
+    { id: 's1', titolo: 'Riscaldamento Pre-Gara', cat: 'Shorts', tag: 'BEHIND', anno: '2026', img: 's_thumb1.png', sliderHome: 'no', HomeRecenti: 'si' },
+    { id: 's2', titolo: 'Esultanza Sotto la Curva', cat: 'Shorts', tag: 'EMOZIONE', anno: '2026', img: 's_thumb2.png', sliderHome: 'si', HomeRecenti: 'si' },
+    { id: 's3', titolo: 'Punto decisivo U16', cat: 'Shorts', tag: 'MATCH POINT', anno: '2026', img: 's_thumb3.png', sliderHome: 'no', HomeRecenti: 'si' },
+    { id: 's4', titolo: 'Trick Shot in Allenamento', cat: 'Shorts', tag: 'FUN', anno: '2026', img: 's_thumb4.png', sliderHome: 'no', HomeRecenti: 'si' }
 ];
