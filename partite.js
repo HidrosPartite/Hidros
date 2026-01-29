8546ef273356dbd8d20fe6fa07efa6bf56ea95ab
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-01-30 00:04:54 +0100

    Update partite.js

--- partite.js
+++ partite.js
@@ -1,6 +1,6 @@
 const DB_PARTITE = [
-    { id: '1', titolo: 'Hidros vs Vero Volley', cat: 'Serie C', tag: 'IN EVIDENZA', anno: '12 Gen 2026', punteggio: '3 - 1', img: 'screenshot.png', sliderHome: 'si', sliderCat: 'si', HomeRecenti: 'si' },
+    { id: '1', titolo: 'Hidros vs Vero Volley', cat: 'Serie C', tag: 'IN EVIDENZA', anno: '12 Gen 2026', punteggio: '3 - 1', img: 'screenshot.png', sliderHome: 'si', sliderCat: 'si', HomeRecenti: 'si', episodi: [{id: '1_set1', titolo: 'Primo Set'}, {id: '1_set2', titolo: 'Secondo Set'}, {id: '1_set3', titolo: 'Terzo Set'}] },
     { id: '2', titolo: 'Hidros vs Lube Civitanova', cat: 'Under 16', tag: 'FINALE', anno: '08 Gen 2026', punteggio: '3 - 2', img: 'screenshot.png', sliderHome: 'no', sliderCat: 'si', HomeRecenti: 'si' },
-    { id: '3', titolo: 'Hidros vs Modena Volley', cat: 'Under 14', tag: 'TOP MATCH', anno: '05 Gen 2026', punteggio: '0 - 3', img: 'screenshot.png', sliderHome: 'si', sliderCat: 'no', HomeRecenti: 'si' },
+    { id: '3', titolo: 'Hidros vs Modena Volley', cat: 'Under 14', tag: 'TOP MATCH', anno: '05 Gen 2026', punteggio: '0 - 3', img: 'screenshot.png', sliderHome: 'si', sliderCat: 'no', HomeRecenti: 'si', episodi: [{id: '3_ep1', titolo: 'Parte 1'}, {id: '3_ep2', titolo: 'Parte 2'}] },
     { id: '4', titolo: 'Hidros vs Trentino Itas', cat: 'Serie C', tag: 'REPLAY', anno: '01 Gen 2026', punteggio: '3 - 0', img: 'screenshot.png', sliderHome: 'no', sliderCat: 'no', HomeRecenti: 'si' }
 ];
