b80473458284dc9e788f705c8daa8301b22bfd2b
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-01 13:00:36 +0100

    Update partite.js

--- partite.js
+++ partite.js
@@ -11,10 +11,10 @@ const DB_PARTITE = [
         sliderCat: 'si', 
         HomeRecenti: 'si', 
         episodi: [
-            {id: 'BAACAgQAAyEFAATHEOJSAAMCaX5xmK_g1rkhWydf3d8__DuedTQAAn8dAALUEfBTh6aPL5JsZaw4BA', titolo: 'Primo Set', img: ''}, 
-            {id: 'BAACAgQAAyEFAATHEOJSAAMLaX8hgz-flJWc1WgMCS1TZTQkk1gAArocAALBIPhT_Gv2obMXpcQ4BA', titolo: 'Secondo Set', img: ''}, 
-            {id: 'BAACAgQAAyEFAATHEOJSAAMMaX8qU0PIn1TZxDDVyy7e6sEGg3gAAr4cAALBIPhTfFYHRoHtyF44BA', titolo: 'Terzo Set', img: ''},
-            {id: 'BAACAgQAAyEFAATHEOJSAAMUaX8vNQmxq2Sf-8OcnpIREVy0VfEAAsIcAALBIPhTWlIXvn6F4W04BA', titolo: 'Terzo Set Parte 2', img: ''}
+            {id: 'BAACAgQAAyEFAATHEOJSAAMCaX5xmK_g1rkhWydf3d8__DuedTQAAn8dAALUEfBTh6aPL5JsZaw4BA', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Primo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAAMLaX8hgz-flJWc1WgMCS1TZTQkk1gAArocAALBIPhT_Gv2obMXpcQ4BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Secondo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAAMMaX8qU0PIn1TZxDDVyy7e6sEGg3gAAr4cAALBIPhTfFYHRoHtyF44BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Terzo_Set.png'},
+            {id: 'BAACAgQAAyEFAATHEOJSAAMUaX8vNQmxq2Sf-8OcnpIREVy0VfEAAsIcAALBIPhTWlIXvn6F4W04BA', titolo: 'Terzo Set Parte 2', img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Terzo_Set_Parte_2.png'}
         ] 
     },
 ];
