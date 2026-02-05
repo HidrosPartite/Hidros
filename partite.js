d3786186e5d4846d923b61965cdfd62f2b9fc0b1
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-05 23:48:59 +0100

    Update partite.js

--- partite.js
+++ partite.js
@@ -17,4 +17,40 @@ const DB_PARTITE = [
             {id: 'BAACAgQAAyEFAATHEOJSAAMUaX8vNQmxq2Sf-8OcnpIREVy0VfEAAsIcAALBIPhTWlIXvn6F4W04BA', titolo: 'Terzo Set Parte 2', img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Terzo_Set_Parte_2.png'}
         ] 
     },
+    { 
+    id: 'NON_INVIARE_QUESTO_SE_HAI_EPISODI', 
+    titolo: 'Hidros vs Aquila Volley Marcianise', 
+    cat: 'Under 16', 
+    tag: 'In Evidenza', 
+    anno: '03 Feb 2026', 
+    punteggio: '3 - 0', 
+    img: 'Copertine_Partite/Hidros_Vs_Aquila_Volley_03-02/Copertina_Principale.png', 
+    sliderHome: 'si', 
+    sliderCat: 'si', 
+    HomeRecenti: 'si', 
+    episodi: [
+        {id: 'BAACAgQAAyEFAATHEOJSAAMbaYL4WbZZyYirLadO69GLNXPoZW8AAoQbAAKUXhlQHD3qSxToV0k4BA', titolo: 'Inizio', img: 'Copertine_Partite/Hidros_Vs_Aquila_Volley_03-02/Inizio.png'}, 
+        {id: 'BAACAgQAAyEFAATHEOJSAAMdaYMrA3v9nEeQ1IvMDie03b4koK8AAq8bAAKUXhlQnrCB9RdyLlE4BA', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_Vs_Aquila_Volley_03-02/Primo_Set.png'}, 
+        {id: 'BAACAgQAAyEFAATHEOJSAAMgaYNcajAsx1SO9GJSWlCVKEHXk2UAAugbAAKUXhlQnHjXEtBLcAs4BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_Vs_Aquila_Volley_03-02/Secondo_Set.png'},
+        {id: 'BAACAgQAAyEFAATHEOJSAAMiaYNiXagaH6CAvTospT2WfgGuDNQAAu0bAAKUXhlQfXhynz_Le984BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_Aquila_Volley_03-02/Terzo_Set.png'}
+    ] 
+},
+    { 
+    id: 'NON_INVIARE_QUESTO_SE_HAI_EPISODI', 
+    titolo: 'Hidros vs Alp Normanna', 
+    cat: 'Under 18', 
+    tag: 'Big Match', 
+    anno: '03 Feb 2026', 
+    punteggio: '3 - 1', 
+    img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Copertina_Principale.png', 
+    sliderHome: 'si', 
+    sliderCat: 'si', 
+    HomeRecenti: 'si', 
+    episodi: [
+        {id: 'BAACAgQAAyEFAATHEOJSAAMraYTureRDtBTXRBtFa2ULjShmTv4AAt8cAAJTKihQD-WlpYJmDCk4BA', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Primo_Set.png'}, 
+        {id: 'BAACAgQAAyEFAATHEOJSAAMtaYT6P5X3VIc4rpuH8_pKDAKZuMcAAuscAAJTKihQZuDkrTcrLPk4BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Secondo_Set.png'}, 
+        {id: 'BAACAgQAAyEFAATHEOJSAAMvaYUCjIrl9P44AAGDWZd3vxqcFWOrAALzHAACUyooUHfaudkajT6iOAQ', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Terzo_Set.png'},
+        {id: 'BAACAgQAAyEFAATHEOJSAAMxaYUPRr30tEW_ELc-r8UXqaVVFJgAAx0AAlMqKFBXq7mVl-8t6jgE', titolo: 'Quarto Set', img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Quarto_Set.png'}
+    ] 
+},
 ];
