8cd9560bd9cad7b346060064b3ac93a88796f936
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-10 21:53:48 +0100

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,12 @@
+//Database
+const DB_PARTITE = [
+    ...DB_U14,
+    ...DB_U15,
+    ...DB_U16,
+    ...DB_U17,
+    ...DB_U18,
+    ...DB_U19,
+    ...DB_PRIMA_DIV,
+    ...DB_SERIED,
+    ...DB_SERIEC
+];
