5377206a40a2573350bd307fa0e32a6511ed2a1c
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-10 21:46:21 +0100

    Update u19.js

--- js/u19.js
+++ js/u19.js
@@ -1,1 +1,20 @@
-
+//Under 19
+const DB_U19 = [
+    { 
+    id: 'u19_virtus_aversa_01', //Hidros vs Virtus Aversa Under 19
+    titolo: 'Hidros vs Virtus Aversa', 
+    cat: 'Under 19', 
+    tag: 'Facile', 
+    anno: '04 Feb 2026', 
+    punteggio: '3 - 0', 
+    img: 'Copertine_Partite/Hidros_Vs_Virtus_Aversa_04-02/Copertina_Principale_04-02.png', 
+    sliderHome: 'si', 
+    sliderCat: 'si', 
+    HomeRecenti: 'si', 
+    episodi: [
+        {id: 'BAACAgQAAyEFAATHEOJSAAM6aYUYfY46wM-lBl1M4083-Sj5DiQAAhwdAAJTKihQsiPJQbEmouU4BA', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_Vs_Virtus_Aversa_04-02/Primo_Set.png'}, 
+        {id: 'BAACAgQAAyEFAATHEOJSAAM8aYXH1VMQxO4-r7GxnN1Fa7i4kjoAAnYdAAJTKihQVREQDj6TS9I4BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_Vs_Virtus_Aversa_04-02/Secondo_Set.png'}, 
+        {id: 'BAACAgQAAyEFAATHEOJSAAM-aYXO2MpvcZh_vmhNUQU4kWEqkBMAAoodAAJTKihQFhFfJb-tNFY4BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_Virtus_Aversa_04-02/Terzo_Set.png'},
+     ] 
+ },
+];
