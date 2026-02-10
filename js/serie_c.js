5e476f689bd49a2b834c4f4aaf691844a581b9dd
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-10 21:40:24 +0100

    Update serie_c.js

--- js/serie_c.js
+++ js/serie_c.js
@@ -1,4 +1,22 @@
 //Serie C
 const DB_SERIEC = [
-    
+       { 
+        id: 'serie_c_casoria_01', //Hidros vs Volley Casoria Serie C 
+        titolo: 'Hidros vs Volley Casoria', 
+        cat: 'Serie C', 
+        tag: 'Rimonta', 
+        anno: '31 Gen 2026', 
+        punteggio: '3 - 2', 
+        img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Copertina_Principale.png', 
+        sliderHome: 'no', 
+        sliderCat: 'si', 
+        HomeRecenti: 'no', 
+        episodi: [
+            {id: 'BAACAgQAAyEFAATHEOJSAAMCaX5xmK_g1rkhWydf3d8__DuedTQAAn8dAALUEfBTh6aPL5JsZaw4BA', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Primo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAAMLaX8hgz-flJWc1WgMCS1TZTQkk1gAArocAALBIPhT_Gv2obMXpcQ4BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Secondo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAAMMaX8qU0PIn1TZxDDVyy7e6sEGg3gAAr4cAALBIPhTfFYHRoHtyF44BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Terzo_Set.png'},
+            {id: 'BAACAgQAAyEFAATHEOJSAAMUaX8vNQmxq2Sf-8OcnpIREVy0VfEAAsIcAALBIPhTWlIXvn6F4W04BA', titolo: 'Terzo Set Parte 2', img: 'Copertine_Partite/Hidros_Vs_Casoria_31-01/Terzo_Set_Parte_2.png'}
+        ] 
+ },
+ 
 ];
