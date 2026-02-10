cd8055b7ea6410f9e39e3644666d1df0bd11d6d5
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-10 22:43:50 +0100

    Update u19.js

--- js/u19.js
+++ js/u19.js
@@ -1,6 +1,24 @@
 //Under 19
 const DB_U19 = [
     { 
+    	id: 'u19_Pallavolo_2000_02', //Hidros vs Pallavolo 2000
+    	titolo: 'Hidros vs Pallavolo 2000', 
+    	cat: 'Under 19', 
+    	tag: 'Antonio MVP', 
+    	anno: '09 Feb 2026', 
+    	punteggio: '3 - 0', 
+    	img: 'Copertine_Partite/Hidros_vs_Pallavolo_2000_09-02/Copertina_Principale_09-02.png', 
+    	sliderHome: 'si', 
+    	sliderCat: 'si', 
+    	HomeRecenti: 'si', 
+    	episodi: [
+            {id: 'BAACAgQAAyEFAATHEOJSAANNaYufLSQ-o11SjYPwqq7-cujeiooAArIaAAKbS1hQDfiwfJaOaAI6BA', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_vs_Pallavolo_2000_09-02/Primo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAANQaYuiHdDj3ON_OTtMKlrEu7I7WzEAArgaAAKbS1hQke4jlfMRfyk6BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_vs_Pallavolo_2000_09-02/Secondo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAANTaYulJvWwH1UckGJQlYyp3lVtZiMAArwaAAKbS1hQVzLDONmjFQI6BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_vs_Pallavolo_2000_09-02/Terzo_Set.png'},
+        ] 
+ },
+    
+    { 
     id: 'u19_virtus_aversa_01', //Hidros vs Virtus Aversa Under 19
     titolo: 'Hidros vs Virtus Aversa', 
     cat: 'Under 19', 
