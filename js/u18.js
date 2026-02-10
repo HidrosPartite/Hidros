c3412d2edd50a1903366567a744d3622dda63cf2
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-10 21:45:26 +0100

    Update u18.js

--- js/u18.js
+++ js/u18.js
@@ -1,1 +1,22 @@
+//Under 18
+const DB_U18 = [
+  { 
+    id: 'u18_alp_normanna_01', //Hidros vs Alp Normanna Under 18
+    titolo: 'Hidros vs Alp Normanna', 
+    cat: 'Under 18', 
+    tag: 'Big Match', 
+    anno: '03 Feb 2026', 
+    punteggio: '3 - 1', 
+    img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Copertina_Principale.png', 
+    sliderHome: 'si', 
+    sliderCat: 'si', 
+    HomeRecenti: 'si', 
+    episodi: [
+        {id: 'BAACAgQAAyEFAATHEOJSAAMraYTureRDtBTXRBtFa2ULjShmTv4AAt8cAAJTKihQD-WlpYJmDCk4BA', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Primo_Set.png'}, 
+        {id: 'BAACAgQAAyEFAATHEOJSAAMtaYT6P5X3VIc4rpuH8_pKDAKZuMcAAuscAAJTKihQZuDkrTcrLPk4BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Secondo_Set.png'}, 
+        {id: 'BAACAgQAAyEFAATHEOJSAAMvaYUCjIrl9P44AAGDWZd3vxqcFWOrAALzHAACUyooUHfaudkajT6iOAQ', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Terzo_Set.png'},
+        {id: 'BAACAgQAAyEFAATHEOJSAAMxaYUPRr30tEW_ELc-r8UXqaVVFJgAAx0AAlMqKFBXq7mVl-8t6jgE', titolo: 'Quarto Set', img: 'Copertine_Partite/Hidros_Vs_Alp_Normanna_03-02/Quarto_Set.png'}
+    ] 
+},  
 
+];
