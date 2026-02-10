3a09687af0c28275739f041076f4bce46ecf1ff5
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-10 21:47:58 +0100

    Create prima_divisione.js

---
+++ js/prima_divisione.js
@@ -0,0 +1,20 @@
+// prima_divisione.js
+const DB_PRIMA_DIV = [
+    { 
+    	id: 'prima_divisione_san_marco_01', //Hidros vs Volley San Marco Prima Divisione
+    	titolo: 'Hidros vs Volley San Marco', 
+    	cat: 'Prima Divisione', 
+    	tag: '3 - 0', 
+    	anno: '08 Feb 2026', 
+    	punteggio: '3 - 0', 
+    	img: 'Copertine_Partite/Hidros_vs_Volley_San_Marco_08-02/Copertina_Principale_08-02.png', 
+    	sliderHome: 'si', 
+    	sliderCat: 'si', 
+    	HomeRecenti: 'si', 
+    	episodi: [
+            {id: 'BAACAgQAAyEFAATHEOJSAANBaYjyd0q2ujpEjtuHbj16jn4RliwAArgaAAII9EhQGnot1v0a4-Y6BA', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_vs_Volley_San_Marco_08-02/Primo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAANFaYj6eiA2z6nBssbiRD4e01p8FukAAtgaAAII9EhQP7nN9OED25Q6BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_vs_Volley_San_Marco_08-02/Secondo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAANIaYkIOyOcAAHeDbyvF4qqXIlkpoqAAALoGgACCPRIUJbGk4LdFDyZOgQ', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_vs_Volley_San_Marco_08-02/Terzo_Set.png'},
+        ] 
+ },
+];
