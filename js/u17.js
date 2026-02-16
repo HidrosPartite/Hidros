f213e6491e75667f02954d1d268cc9b696737c2e
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-16 11:56:40 +0100

    Update u17.js

--- js/u17.js
+++ js/u17.js
@@ -1,21 +1,21 @@
 //Under 17
 const DB_U17 = [
-  id: 'u17_carbat_01', //Hidros vs Carbat Matese Under 17
-    	titolo: 'Hidros vs Carbat Matese', 
-    	cat: 'Under 17', 
-    	tag: '1 - 3', 
-    	anno: '12 Feb 2026', 
-    	punteggio: '1 - 3', 
-    	img: 'Copertine_Partite/Hidros_Vs_Carbat_Matese_12-02/Copertina_Principale_12-02.png', 
-    	sliderHome: 'no', 
-    	sliderCat: 'si', 
-    	HomeRecenti: 'si', 
-    	episodi: [
+    {
+        id: 'u17_carbat_01', 
+        titolo: 'Hidros vs Carbat Matese', 
+        cat: 'Under 17', 
+        tag: '1 - 3', 
+        anno: '12 Feb 2026', 
+        punteggio: '1 - 3', 
+        img: 'Copertine_Partite/Hidros_Vs_Carbat_Matese_12-02/Copertina_Principale_12-02.png', 
+        sliderHome: 'no', 
+        sliderCat: 'si', 
+        HomeRecenti: 'si', 
+        episodi: [
             {id: 'BAACAgQAAyEFAATHEOJSAANWaZCUmpfI4Kf6L0L4zt9agPMbyAADlBsAAulygFBtoHYDHJWGMDoE', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_Vs_Carbat_Matese_12-02/Primo_Set.png'}, 
             {id: 'BAACAgQAAyEFAATHEOJSAANYaZGyYlFomm5UNQwXoALgjgaPGO0AAq8dAAJy_pFQ3NpKsD66xEk6BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_Vs_Carbat_Matese_12-02/Secondo_Set.png'}, 
             {id: 'BAACAgQAAyEFAATHEOJSAANaaZHIAV2KV6FHIQheIryxPHy4yRsAAsIdAAJy_pFQNHXyXv7AuBA6BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_Carbat_Matese_12-02/Terzo_Set.png'},
-            {id: 'BAACAgQAAyEFAATHEOJSAANcaZLepu0CWB1gRayTuYw3yxD0CjEAArYcAAIVLphQzNBFXW_ATuQ6BA', titolo: 'Quarto Set', img: 'Copertine_Partite/Hidros_Vs_Carbat_Matese_12-02/Quarto_Set.png'},
+            {id: 'BAACAgQAAyEFAATHEOJSAANcaZLepu0CWB1gRayTuYw3yxD0CjEAArYcAAIVLphQzNBFXW_ATuQ6BA', titolo: 'Quarto Set', img: 'Copertine_Partite/Hidros_Vs_Carbat_Matese_12-02/Quarto_Set.png'}
         ] 
- },
-  
+    }
 ];
