2a570a01e7458ba00ac2dad4dd9ec786e524244b
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-17 23:37:48 +0100

    Update u16.js

--- js/u16.js
+++ js/u16.js
@@ -1,6 +1,24 @@
 //Under 16
 const DB_U16 = [
     { 
+    	id: 'u16_volleytime_02', //Hidros vs VolleyTime Blu Under 16
+    	titolo: 'Hidros vs VolleyTime Blu', 
+    	cat: 'Under 16', 
+    	tag: 'In Evidenza',
+    	anno: '16 Feb 2026', 
+    	punteggio: '1 - 3', 
+    	img: 'Copertine_Partite/Hidros_Vs_VolleyTime_Blu_16-02/Copertina_Principale_16-02.png', 
+    	sliderHome: 'si', 
+    	sliderCat: 'si', 
+    	HomeRecenti: 'si', 
+    	episodi: [
+            {id: 'BAACAgQAAyEFAATHEOJSAANkaZTKJZSezUz4dpuF599GZRKZuF0AAu8cAAIVLqhQn1CAN_YsXYk6BA', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_Vs_VolleyTime_Blu_16-02/Primo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAANmaZTQQ_eodnzEbYwCvI45a-i1bJIAAvEcAAIVLqhQ3whz6B1-LS46BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_Vs_VolleyTime_Blu_16-02/Secondo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAANoaZTfb4BFSak_ID7qOl2_TvYglPkAAtweAAIc5qhQHhgjUQS8A8U6BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_VolleyTime_Blu_16-02/Terzo_Set.png'},
+            {id: 'BAACAgQAAyEFAATHEOJSAANqaZTi71ZzDTQEMuSH6sr7MBDfDEgAAuceAAIc5qhQDCIg8LIi9kY6BA', titolo: 'Quarto Set', img: 'Copertine_Partite/Hidros_Vs_VolleyTime_Blu_16-02/Quarto_Set.png'}
+    ] 
+},
+    { 
     	id: 'u16_marcianise_01', //Hidros vs Aquila Volley Marcianise Under 16
     	titolo: 'Hidros vs Aquila Volley Marcianise', 
     	cat: 'Under 16', 
@@ -18,5 +36,4 @@ const DB_U16 = [
             {id: 'BAACAgQAAyEFAATHEOJSAAMiaYNiXagaH6CAvTospT2WfgGuDNQAAu0bAAKUXhlQfXhynz_Le984BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_Aquila_Volley_03-02/Terzo_Set.png'}
     ] 
 },
-
 ];
