503a1140ae00ec8e915e93a3e36af02d2efd7790
HidrosPartite <hidrostelegrampartite@gmail.com>
2026-02-19 22:31:27 +0100

    Update u17.js

--- js/u17.js
+++ js/u17.js
@@ -1,6 +1,23 @@
 //Under 17
 const DB_U17 = [
     {
+        id: 'u17_ortav_01', 
+        titolo: 'Hidros vs Orta Verda', 
+        cat: 'Under 17', 
+        tag: 'Easy', 
+        anno: '18 Feb 2026', 
+        punteggio: '3 - 0', 
+        img: 'Copertine_Partite/Hidros_Vs_OrtaV_19-02/Copertina_Principale_18-02.png', 
+        sliderHome: 'si', 
+        sliderCat: 'si', 
+        HomeRecenti: 'si', 
+        episodi: [
+            {id: 'BAACAgQAAyEFAATHEOJSAAN6aZZGSlv-fYSgAAEsNGrM8_RU2P8BAAKZGwACHOa4UPcbIfk6UnjPOgQ', titolo: 'Primo Set', img: 'Copertine_Partite/Hidros_Vs_OrtaV_19-02/Primo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAAOCaZdT7aMsaMOxQdekB9C58iWOlQEAAh8dAALvf7hQxIYrnV5RsIc6BA', titolo: 'Secondo Set', img: 'Copertine_Partite/Hidros_Vs_OrtaV_19-02/Secondo_Set.png'}, 
+            {id: 'BAACAgQAAyEFAATHEOJSAAOEaZdaPctCYhQTSg_6LgSVdNiPK8YAAiEdAALvf7hQho3l7UaYihg6BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_OrtaV_19-02/Terzo_Set.png'},
+        ] 
+    },
+    {
         id: 'u17_carbat_01', 
         titolo: 'Hidros vs Carbat Matese', 
         cat: 'Under 17', 
@@ -17,5 +34,5 @@ const DB_U17 = [
             {id: 'BAACAgQAAyEFAATHEOJSAANaaZHIAV2KV6FHIQheIryxPHy4yRsAAsIdAAJy_pFQNHXyXv7AuBA6BA', titolo: 'Terzo Set', img: 'Copertine_Partite/Hidros_Vs_Carbat_Matese_12-02/Terzo_Set.png'},
             {id: 'BAACAgQAAyEFAATHEOJSAANcaZLepu0CWB1gRayTuYw3yxD0CjEAArYcAAIVLphQzNBFXW_ATuQ6BA', titolo: 'Quarto Set', img: 'Copertine_Partite/Hidros_Vs_Carbat_Matese_12-02/Quarto_Set.png'}
         ] 
-    }
+    },
 ];
